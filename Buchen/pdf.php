5cc242e5f80517576c3522b408fc4964478d13d7
stefan.maximilian.dietrich@gmail.com <stefan.maximilian.dietrich@gmail.com>
2022-03-24 16:20:23 +0100

    Resourcen auslagern 2.0

--- Buchen/pdf.php
+++ Buchen/pdf.php
@@ -8,7 +8,7 @@ $rechnungs_datum = date("d.m.Y");
 $pdfAuthor = "schwimmschule-allgaeu.de";
 
 $rechnungs_header = '
-<img src="logo.png">
+<img src="/../Resourcen/logo_schwimmschule.png">
 Schwimmschule Allgäu
 Alfrd Hartmann
 schwimmschule-allgaeu.de';
