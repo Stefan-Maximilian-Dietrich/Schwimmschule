b749f289e02bbfca2e0a65da8e9a1d1a0c8b8e9b
stefan.maximilian.dietrich@gmail.com <stefan.maximilian.dietrich@gmail.com>
2022-03-24 20:54:27 +0100

    versuch 9

--- Dialog/termine.php
+++ Dialog/termine.php
@@ -4,4 +4,4 @@ $_SESSION["mail"] = $_GET["mail"];
 echo $_SESSION["mail"] ;
 ?>
 
-<meta http-equiv="refresh" content="0; URL= termine2.php" />
+<meta http-equiv="refresh" content="0; URL=/../Dialog/termine2.php" />
