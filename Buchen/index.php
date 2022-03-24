e1f02f869c346e6785ae83971a9fabeace1874d1
stefan.maximilian.dietrich@gmail.com <stefan.maximilian.dietrich@gmail.com>
2022-03-24 16:13:57 +0100

    Resourcen azslagern 1.2

--- Buchen/index.php
+++ Buchen/index.php
@@ -102,7 +102,7 @@ if ($_GET["page"] == "log") {
 
 
                     <div class="logo">
-                        <img src="logo_schwimmschule.png" alt="logo" hight="30%" width="30%">
+                        <img src="TESTlogo_schwimmschule.png" alt="logo" hight="30%" width="30%">
                     </div>
 
                     <h2>Wilkommen</h2>
